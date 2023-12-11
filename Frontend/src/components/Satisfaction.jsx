f6512ea720321163c24689972a360cfeeb7c4824
huzeyfa welela <117175344+huuzey@users.noreply.github.com>
2023-12-11 20:15:14 +0300

    Update Satisfaction.jsx

--- Frontend/src/components/Satisfaction.jsx
+++ Frontend/src/components/Satisfaction.jsx
@@ -14,7 +14,7 @@ const Satisfaction = (props) => {
           fun(value);
         }}
         className={` ${
-          back ? "bg-[#d14da3]" : ""
+          back ? "bg-[rgb(203,49,92)]" : ""
         } text-white border-2  w-3/4 flex items-center  justify-center sm:text-[3px] md:text-[10px] sm:font-light md:font-semibold py-1 border-black bg-[#32a8a4]   rounded-full`}
       >
         {title}
