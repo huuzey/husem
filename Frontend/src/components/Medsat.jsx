ce247a9cefb9d48875bc1694df133aa6bc8cbd4a
huzeyfa welela <117175344+huuzey@users.noreply.github.com>
2023-12-11 19:47:19 +0300

    Update Medsat.jsx

--- Frontend/src/components/Medsat.jsx
+++ Frontend/src/components/Medsat.jsx
@@ -14,8 +14,8 @@ const Satisfaction = (props) => {
           fun(value);
         }}
         className={`${
-          back ? "bg-[#c40a7d]" : ""
-        }   text-white border-2  sm:px-1 md:px-0 flex items-center  justify-center text-[9px] md:text-[10px] sm:font-light md:font-semibold py-1 border-black bg-[#32a8a4]   rounded-full`}
+          back ? "bg-[#d14da3]" : ""
+        }   text-white border-2 w-3/4  sm:px-1 md:px-0 flex items-center  justify-center text-[9px] md:text-[10px] sm:font-light md:font-semibold py-1 border-black bg-[#32a8a4]   rounded-full`}
       >
         {title}
       </button>
